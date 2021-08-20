2b33ddaebfd169c772275f407e2b695ba07df0a4
haha2313 <83167333+haha2313@users.noreply.github.com>
2021-08-20 11:44:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/kenzok8/small-package package/openwrt-package
+git clone https://github.com/kenzok8/small package/openwrt-small
